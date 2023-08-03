fe6d2e809bad5b6fb34eae500ae3964598de2596
Tendry24 <hei.tendry.2@gmail.com>
2023-08-03 07:37:38 +0300

    Feat : Initialize the project

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from "react";
+import TodoApp from "./TodoApp/TodoApp"; 
+
+function App() {
+  return (
+    <div className="App">
+      <TodoApp />
+    </div>
+  );
+}
+
+export default App;
+
+/*import logo from './logo.svg';
+import './App.css';
+
+
+function Hello(){
+  return(
+    <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn this Libraries
+        </a>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Hello/>
+      </header>
+    </div>
+  );
+}
+
+
+
+export default App;
+*/
---
+++ src/todosData.js
@@ -0,0 +1,6 @@
+const todosData = [
+    { id: 1, task: "Faire les courses", completed: false },
+  ];
+  
+  export default todosData;
+  
